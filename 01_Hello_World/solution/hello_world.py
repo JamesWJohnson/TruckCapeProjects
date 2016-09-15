046808f67fea113139090f7e4c8f78e219034bdd
James Johnson <clayshooter811@gmail.com>
2016-09-15 01:41:28 +0000

    add first exercise

---
+++ 01_Hello_World/solution/hello_world.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Hello, and welcome to your first Python program!
+
+# Lines beginning with a '#' are called comments, and they are ignored by the compiler
+# They're used by programmers to leave notes about the code, to help other programmers
+# (or themselves) understand the code better.
+
+# Comments begin anywhere after a #, so you can have a comment begin after valid code
+
+to_print = "Hello World!" #See?
+
+# In the above line, we declared a variable called to_print and assigned it the value
+# "Hello World!" Notice how, unlike in our Arduino code, we didn't need to give it a
+# type. That's because Python uses something called "dynamic typing" where the
+# interpreter figures out what the type of something is at runtime.
+
+# Now we're going to do something called a "function call." Function calls take the form
+# function_name(a, b, c,) where a, b, and c are arguments.
+
+# For your first assignment, call the function "print" with a single argument, "to_print"
+
+print(to_print)
